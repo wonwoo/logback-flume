66a4c8908159279d49db9d151b21d64ce39dc137
wonwoo <aoruqjfu@gmail.com>
2017-08-17 15:39:56 +0900

    필요 없는 테스트 삭제

--- redis/src/test/java/me/wonwoo/appender/FlumeRedisAppenderTests.java
+++ redis/src/test/java/me/wonwoo/appender/FlumeRedisAppenderTests.java
@@ -1,12 +1,6 @@
 package me.wonwoo.appender;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import me.wonwoo.flume.channel.ChannelAttr;
 import me.wonwoo.sink.FlumeRedisSink;
@@ -28,17 +22,17 @@ public class FlumeRedisAppenderTests {
     assertThat(sink).isNotNull();
   }
 
-  private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-  @Test
-  public void appenderRedisError() {
-    try {
-      URL uri = new URL("urltest");
-      uri.openConnection();
-    } catch (MalformedURLException e) {
-      logger.error("url formed exception ", e);
-    } catch (IOException e) {
-      logger.error("error {} : ", e.toString());
-    }
-  }
+//  private Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//  @Test
+//  public void appenderRedisError() {
+//    try {
+//      URL uri = new URL("urltest");
+//      uri.openConnection();
+//    } catch (MalformedURLException e) {
+//      logger.error("url formed exception ", e);
+//    } catch (IOException e) {
+//      logger.error("error {} : ", e.toString());
+//    }
+//  }
 }
