0f00d13c712ec9386b8e4cae046292e48eb03e78
wonwoo <aoruqjfu@gmail.com>
2017-08-17 10:47:15 +0900

    kafka test 추가

--- kafka/src/test/java/me/wonwoo/sink/FlumeKafkaSinkTests.java
+++ kafka/src/test/java/me/wonwoo/sink/FlumeKafkaSinkTests.java
@@ -1,12 +1,41 @@
 package me.wonwoo.sink;
 
+import org.apache.flume.Sink;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import me.wonwoo.flume.channel.ChannelAttr;
+import net.manub.embeddedkafka.EmbeddedKafka$;
+import net.manub.embeddedkafka.EmbeddedKafkaConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class FlumeKafkaSinkTests {
-	//TODO
+
+	@Before
+	public void setup() {
+		EmbeddedKafka$.MODULE$.start(EmbeddedKafkaConfig.defaultConfig());
+	}
+
 	@Test
 	public void createSink() {
-
+		KafkaAttr kafkaAttr = new KafkaAttr();
+		kafkaAttr.setTopic("test");
+		kafkaAttr.setBrokerList("localhost:9092");
+		ChannelAttr channelAttr = new ChannelAttr();
+		channelAttr.setDataDirs("/Users/wonwoolee/Desktop/flume");
+		channelAttr.setCheckpointDir("/Users/wonwoolee/Desktop/flume/checkout");
+		FlumeKafkaSink flumeKafkaSink = new FlumeKafkaSink("test", "channelTest", kafkaAttr, channelAttr);
+		Sink sink = flumeKafkaSink.createSink();
+		assertThat(sink.getName()).contains("test");
+		assertThat(sink.getLifecycleState().name()).isEqualTo("IDLE");
 	}
+
+	@After
+	public void close() {
+		EmbeddedKafka$.MODULE$.stop();
+	}
+
 }
